5e7064d799629708559d2de2adcc7c0835634434
Altynbek <altuxadark98@gmail.com>
2022-05-05 19:06:10 +0600

    go

--- main.go
+++ main.go
@@ -77,3 +77,49 @@ func startClient(addr string) {
 		fmt.Printf("Connection error: %s\n", err)
 	}
 }
+
+// package main
+
+// import "net"
+// import "fmt"
+// import "bufio"
+// import "strings" // требуется только ниже для обработки примера
+
+// func main() {
+
+//   fmt.Println("Launching server...")
+
+//   // Устанавливаем прослушивание порта
+//   ln, _ := net.Listen("tcp", ":8081")
+
+//   // Открываем порт
+//   conn, _ := ln.Accept()
+
+//   // Запускаем цикл
+//   for {
+//     // Будем прослушивать все сообщения разделенные \n
+//     message, _ := bufio.NewReader(conn).ReadString('\n')
+//     // Распечатываем полученое сообщение
+//     fmt.Print("Message Received:", string(message))
+//     // Процесс выборки для полученной строки
+//     newmessage := strings.ToUpper(message)
+//     // Отправить новую строку обратно клиенту
+//     conn.Write([]byte(newmessage + "\n"))
+//   }
+// }
+
+// func Socket() {
+// 	// Подключаемся к сокету
+// 	conn, _ := net.Dial("tcp", "127.0.0.1:8080")
+// 	for {
+// 		// Чтение входных данных от stdin
+// 		reader := bufio.NewReader(os.Stdin)
+// 		fmt.Print("Text to send: ")
+// 		text, _ := reader.ReadString('\n')
+// 		// Отправляем в socket
+// 		fmt.Fprintf(conn, text+"\n")
+// 		// Прослушиваем ответ
+// 		message, _ := bufio.NewReader(conn).ReadString('\n')
+// 		fmt.Print("Message from server: " + message)
+// 	}
+// }
